5947f655bf8db444bee4f221f0d9c7bed675322f
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-06-03 03:18:55 -0700

    BF solution for Ruby

--- 03_Problem/Ruby/ruby.rb
+++ 03_Problem/Ruby/ruby.rb
@@ -1,24 +1,52 @@
-factors = [];
-primes = [];
-final = [];
+$factors =[];
+$primes = [1];
+$final = [];
 
-
-
-
-
-
-
-i = 1
-j = 1000
-while j > 0 do
-    num = j
-    while i < num do
+def factor(num)
+    i = 1
+    while i <= num do
         if num % i == 0
-            factors.push(i)
+            $factors.push(i)
         end
         i += 1
     end
-    j -= 1
+    if $factors.length == 2
+        $primes.push($factors[1])
+    end
+    $factors.clear
+end 
+
+
+def prime(x)
+    while x > 0 do
+        factor(x)
+        x -= 1
+    end
+    return $primes
 end
 
-puts factors
+def solve(input)
+    j = 0
+    while j < input.length do
+        if 600851475143 % input[j] == 0
+            $final.push(input[j])
+        end
+        j += 1
+    end
+end
+
+def largest(array)
+    largest = 0
+    z = 0
+    while z < array.length do
+        if array[z] > largest
+            largest = array[z]
+        end
+        z += 1
+    end
+    return puts largest
+end
+
+prime(10000).sort()
+solve($primes)
+largest($final)
